96e848618139cfdf35fb3ba9cab37a0b6a527f03
Neal Chandra <nealxchandra@gmail.com>
2025-06-01 21:05:41 -0700

    Downloaded data, added some simple preprocessing scripts to get training data

---
+++ data.py
@@ -0,0 +1,25 @@
+import torch
+import torchaudio 
+import torchvision
+from torchaudio.transforms import Spectrogram, InverseSpectrogram
+
+file_names = [e for e in os.walk('data')][0][2]
+dir_name = 'data'
+n_fft = 1024
+
+data = []
+for file in tqdm(file_names[:30]):
+    waveform, sample_rate = torchaudio.load(f"{dir_name}/{file}")
+    data.append(waveform.mean(dim=0, keepdim=True))
+
+SAMPLE_RATE = sample_rate
+transform = Spectrogram(n_fft=n_fft, power=None)
+inv_transform = InverseSpectrogram(n_fft=n_fft, power=None)
+
+X = [] #actual data 
+seq_len = 100
+for e in data:
+    e = transform(e)[0]
+    for i in range(0, e.shape[1], seq_len):
+        X.append(e[:, i:i+seq_len])
+
