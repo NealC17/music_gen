ab529bd936994c9063698d42891fc7f96495df1c
Neal Chandra <nealxchandra@gmail.com>
2025-06-29 18:29:57 -0700

    added device agnostic code, more datapoints via conv, running into lacking of memory issue

--- data.py
+++ data.py
@@ -5,12 +5,19 @@ from torchaudio.transforms import Spectrogram, InverseSpectrogram
 from tqdm import tqdm 
 import os 
 
+if torch.backends.mps.is_available():
+    device = torch.device("mps")  
+elif torch.cuda.is_available():
+    device = torch.device("cuda")  
+else:
+    device = torch.device("cpu")  
+
 file_names = [e for e in os.walk('data')][0][2]
 dir_name = 'data'
 n_fft = 1024
 
 data = []
-for file in tqdm(file_names[:30]):
+for file in tqdm(file_names[:10]):
     waveform, sample_rate = torchaudio.load(f"{dir_name}/{file}")
     data.append(waveform.mean(dim=0, keepdim=True))
 
@@ -19,9 +26,11 @@ transform = Spectrogram(n_fft=n_fft, power=None)
 inv_transform = InverseSpectrogram(n_fft=n_fft)
 
 X = [] #actual data 
+y = []
 seq_len = 100
 for e in tqdm(data):
     e = transform(e)[0]
-    for i in range(0, e.shape[1], seq_len):
-        X.append(e[:, i:i+seq_len])
+    for i in range(0, e.shape[1] - seq_len):
+        X.append(e[:, i:i+seq_len].to(device))
+        y.append(e[:, i+seq_len].to(device))
 
